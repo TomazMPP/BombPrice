35aae41739734620364b753d6983f736d38e2c12
TomazMPP <tomazmppontes@gmail.com>
2024-03-31 10:21:29 -0300

    🐛 fix nftnumber

--- src/components/Validation.vue
+++ src/components/Validation.vue
@@ -74,10 +74,13 @@ export default {
      const regex = /^https:\/\/opensea\.io\/assets\/matic\/(0xd8a06936506379dbBe6e2d8aB1D8C96426320854|0x2d5f4ba3e4a2d991bd72edbf78f607c174636618)\/\d+\/?$/i;
      
         if (regex.test(this.openSeaLink)) {
-            const nftNumber = this.openSeaLink.split('/').pop();
-            
+          const parts = this.openSeaLink.split('/');
+const nftNumber = parts[parts.length - 1].replace('/', '');
+          
             if (nftNumber.endsWith('/')) {
+              console.log(nftNumber)
             nftNumber = nftNumber.slice(0, -1);
+            console.log(nftNumber)
         }
             if (!isNaN(nftNumber)) {
                 this.validNFT = true;
