8fb361e2a9c7c4f957689aa9218ae01b39da10be
TomazMPP <tomazmppontes@gmail.com>
2024-03-31 00:18:48 -0300

    Update Bonecos.vue

--- src/components/Bonecos.vue
+++ src/components/Bonecos.vue
@@ -14,12 +14,8 @@ export default {
     this.fetchListings();
   },
   methods: {
-    mapAbilityIcon(ability) {
-    return `https://bcrypt.com.br/_next/image?url=%2Fatts%2F${ability}_icon.png&w=48&q=75`;
-  },
     getSquareClass(listing) {
         const formattedPrice = this.getPrice(listing);
-
       if (listing.bcryptData && listing.bcryptData.rarity === 'Legend') {
         return 'quadradoLendario';
       } else if (listing.bcryptData && listing.bcryptData.rarity === 'Super Legend') {
@@ -38,6 +34,7 @@ export default {
         const data = await response.json();
         this.listings = data.listings;
         
+        // Iniciar o processo de busca do Bcrypt Data
         await this.fetchBcryptData();
       } catch (error) {
         console.error('Erro ao buscar listagens:', error);
@@ -45,7 +42,7 @@ export default {
     },
     async fetchBcryptDataOnClick() {
       
-    const fieldsToFetch = ['rarity', 'level', 'image', 'abilities' ]; 
+    const fieldsToFetch = ['rarity', 'level', 'battery', 'image']; // Include 'image' field
     const rarityMap = {
         'C': 'Common',
         'R': 'Rare',
@@ -54,46 +51,37 @@ export default {
         'L': 'Legend',
         'SL': 'Super Legend'
     };
-
+    const batteryMap = {
+        '0': 'No',
+        '0.5': 'Yes'
+    };
     for (let i = 0; i < this.listings.length; i++) {
         const listing = this.listings[i];
         const identifier = this.getIdentifier(listing);
-
       
-        try {
-    this.showMain = true;
-    const response = await fetch(`https://nft.bcrypt.com.br/bhero/pol/${identifier}`);
-    const data = await response.json();
-
-    if (data.hasOwnProperty('rarity') && rarityMap.hasOwnProperty(data.rarity)) {
-        data.rarity = rarityMap[data.rarity];
-    }
-
-    const filteredData = {};
-    fieldsToFetch.forEach(field => {
-        if (data.hasOwnProperty(field)) {
-            filteredData[field] = data[field];
+      try {
+        this.showMain = true;
+            const response = await fetch(`https://nft.bcrypt.com.br/bhero/pol/${identifier}`);
+            const data = await response.json();
+            if (data.hasOwnProperty('rarity') && rarityMap.hasOwnProperty(data.rarity)) {
+                data.rarity = rarityMap[data.rarity];
+            }
+            if (data.hasOwnProperty('battery') && batteryMap.hasOwnProperty(data.battery.toString())) {
+                data.battery = batteryMap[data.battery.toString()];
+            }
+            const filteredData = {};
+            fieldsToFetch.forEach(field => {
+                if (data.hasOwnProperty(field)) {
+                    filteredData[field] = data[field];
+                }
+            });
+            if (filteredData.hasOwnProperty('image') && !filteredData.image.startsWith('http')) {
+                filteredData.image = `https://bcrypt.com.br/_next/image?url=%2Fbhero%2F${filteredData.image}&w=128&q=75`; 
+            }
+            this.listings[i].bcryptData = filteredData;
+        } catch (error) {
+            console.error('Error fetching Bcrypt data:', error);
         }
-    });
-
-    if (filteredData.hasOwnProperty('image') && !filteredData.image.startsWith('http')) {
-        filteredData.image = `https://bcrypt.com.br/_next/image?url=%2Fbhero%2F${filteredData.image}&w=128&q=75`;
-    }
-
-    if (data.hasOwnProperty('abilities')) {
-        this.listings[i].bcryptData.abilities = filteredData.abilities.map(ability => this.mapAbilityIcon(ability));
-
-    }
-
-    if (!this.listings[i].bcryptData) {
-        this.listings[i].bcryptData = {};
-    }
-
-    this.listings[i].bcryptData = filteredData;
-} catch (error) {
-    console.error('Error fetching Bcrypt data:', error);
-}
-
         await this.delay(1000);
     }
 },
@@ -105,7 +93,7 @@ export default {
     },
     getPrice(listing) {
       const { currency, value } = listing.price.current;
-      let formattedPrice = parseFloat(value) / Math.pow(10, 18); // Convertendo para Matic
+      let formattedPrice = parseFloat(value) / Math.pow(10, 18);
       return `${formattedPrice} ${currency}`;
     },
     mapRarityClass(rarity) {
@@ -113,10 +101,7 @@ export default {
     },
   },
 };
-
 </script>
-
-
 <template>
  <div :class="{ 'botaoFetch': true, 'removeBorderRadius': showMain }">
     
@@ -136,21 +121,12 @@ To get the heroes data, you must click the button above</p>
                     <template v-if="key !== 'image'">
                         <strong>{{ key }}:</strong> {{ value }}
                     </template> 
-                    <template v-else-if="key === 'abilities'">
-                <strong>{{ key }}:</strong>
-                <div class="abilities">
-                  <img v-for="(ability, index) in value" :key="index" v-bind:src="ability" :alt="ability">
-                </div>
-            </template>
                 </li>
-                
             </ul>
-            
-</div>
             <p><a :href="'https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/' + getIdentifier(listing)" target="_blank" class="linkOpen"><u>OpenSea</u></a></p>
         </div>
     </div>
-
+</div>
   </template>
   
   
@@ -193,7 +169,6 @@ padding: 50px;
     border-bottom-left-radius: 10px;
     justify-content: center;
 }
-
 .quadradoInfo {
   background-color: #b5c6da;
   color: rgb(0, 0, 0);
@@ -202,7 +177,6 @@ padding: 50px;
   text-align: left;
   width: 220px;
 }
-
 ul, li {
     list-style: none;
     text-align: left;
@@ -213,40 +187,39 @@ ul, li {
 .quadradoInfo p {
   margin: 0;
 }
-
+.quadradoLendario {
+  background-color: rgb(161, 97, 12);
 .quadradoSRaro {
   background-color: #735874 !important;
 }
 .quadradoRaro {
   background-color: #2c852c !important;
-
+.quadradoSuperLenda {
+  background-color: blue;
 }
 .quadradoEpico {
   background-color: #8b2a8f !important;
   color: white !important;
-
 }
 .quadradoLendario {
   background-color: rgb(161, 97, 12) !important;
 }
-
+.profit {
+    background-color: green;
 .quadradoSuperLenda {
   background-color: #ff0759 !important;
   color: white !important;
 }
-
 .linkOpen {
     text-decoration: none;
     color: #521f1f;
 }
-
 @media screen and (max-width: 1200px) {
     .main {
         grid-template-columns: repeat(3, minmax(250px, 1fr)); /* 3 itens por linha */
        
     }
 }
-
 @media screen and (max-width: 800px) {
     .main {
         grid-template-columns: repeat(2, minmax(300px, 1fr)); /* 2 itens por linha */
@@ -255,7 +228,6 @@ ul, li {
         
     }
 }
-
 @media screen and (max-width: 600px) {
     .main {
       margin: 0;
