9988bb0eb7a55876395bb43d5caf3ea8cd81864d
TomazMPP <tomazmppontes@gmail.com>
2024-04-11 22:49:28 -0300

    Update Check.vue

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -3,11 +3,79 @@
  <p class="warning">
 This page showcases the heroes available for purchase from the site owner,<br>always offered at competitive prices and ensuring a safe transaction.</p>
     <div class="main">
+      <div class="quadradoEpico quadradoInfo">
+       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fking_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
+       <p><strong>ID: </strong>1329468</p>
+       <p><strong>Rarity: </strong>Epic</p>
+       <p><strong>Price: </strong>7 MATIC</p>
+       <div class="skills">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ftreasure_hunter_icon.png&w=48&q=75" alt="Treasure Hunter Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fsave_battery_icon.png&w=48&q=75" alt="Save Battery Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ffast_charge_icon.png&w=48&q=75" alt="Battery Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fbomb_pass_icon.png&w=48&q=75" alt="Bomb Pass Skill">
+
+    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
+  </div>
+    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/1329468" target="_blank">BUY NOW! (OPENSEA)</a>
+      </div>
+      <div class="quadradoEpico quadradoInfo">
+       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Ftiger_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
+       <p><strong>ID: </strong>835570</p>
+       <p><strong>Rarity: </strong>Epic</p>
+       <p><strong>Price: </strong>7 MATIC</p>
+       <div class="skills">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ftreasure_hunter_icon.png&w=48&q=75" alt="Treasure Hunter Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fsave_battery_icon.png&w=48&q=75" alt="Save Battery Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fbomb_pass_icon.png&w=48&q=75" alt="Bomb Pass Skill">
+      <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fblock_pass_icon.png&w=48&q=75" alt="Block Pass Skill">
+    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
+  </div>
+    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/835570" target="_blank">BUY NOW! (OPENSEA)</a>
+      </div>
+      <div class="quadradoSRaro quadradoInfo">
+       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fninja_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
+       <p><strong>ID: </strong>418411</p>
+       <p><strong>Rarity: </strong>Super Rare</p>
+       <p><strong>Price: </strong>2.5 MATIC</p>
+       <div class="skills">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ftreasure_hunter_icon.png&w=48&q=75" alt="Treasure Hunter Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fjail_breaker_icon.png&w=48&q=75" alt="Jail Breaker Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fsave_battery_icon.png&w=48&q=75" alt="Save Battery Skill">
+    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
+  </div>
+    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/418411" target="_blank">BUY NOW! (OPENSEA)</a>
+      </div>
+      <div class="quadradoSRaro quadradoInfo">
+       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fvampire_green_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
+       <p><strong>ID: </strong>962565</p>
+       <p><strong>Rarity: </strong>Super Rare</p>
+       <p><strong>Price: </strong>2.5 MATIC</p>
+       <div class="skills">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ftreasure_hunter_icon.png&w=48&q=75" alt="Treasure Hunter Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fjail_breaker_icon.png&w=48&q=75" alt="Jail Breaker Skill">
+      <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fpierce_block_icon.png&w=48&q=75" alt="Pierce Block Skill">
+    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
+  </div>
+    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/962565" target="_blank">BUY NOW! (OPENSEA)</a>
+      </div>
+      <div class="quadradoSRaro quadradoInfo">
+       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fninja_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
+       <p><strong>ID: </strong>696689</p>
+       <p><strong>Rarity: </strong>Super Rare</p>
+       <p><strong>Price: </strong>2.5 MATIC</p>
+       <div class="skills">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fjail_breaker_icon.png&w=48&q=75" alt="Jail Breaker Skill">
+      <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ffast_charge_icon.png&w=48&q=75" alt="Battery Skill">
+      <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fbomb_pass_icon.png&w=48&q=75" alt="Bomb Pass Skill">
+    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
+  </div>
+    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/696689" target="_blank">BUY NOW! (OPENSEA)</a>
+      </div>
       <div class="casa quadradoInfo">
        <img src="https://bcrypt.com.br/_next/image?url=%2Fbhouse%2FTinyHouse.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
        <p><strong>ID: </strong>121</p>
        <p><strong>Rarity: </strong>Tiny House</p>
-       <p><strong>Price: </strong>35 MATIC</p>
+       <p><strong>Price: </strong>45 MATIC</p>
        <div class="skills"><br></div>
     <a class="buyButton" href="https://opensea.io/assets/matic/0x2d5f4ba3e4a2d991bd72edbf78f607c174636618/121" target="_blank">BUY NOW! (OPENSEA)</a>
       </div>
@@ -59,7 +127,7 @@ This page showcases the heroes available for purchase from the site owner,<br>al
 }
 .main {
     display: grid;
-  grid-template-columns: repeat(4, minmax(0, 2fr));
+  grid-template-columns: repeat(3, minmax(0, 2fr));
   grid-auto-rows: auto;
   grid-auto-flow: dense;
   gap: 50px;
@@ -68,6 +136,19 @@ padding: 50px;
   border-bottom-right-radius: 10px;
     border-bottom-left-radius: 10px;
     justify-content: center;
+}
+
+.quadradoSRaro {
+  background-color: #735874 !important;
+}
+.quadradoRaro {
+  background-color: #2c852c !important;
+
+}
+.quadradoEpico {
+  background-color: #8b2a8f !important;
+  color: white !important;
+
 }
 
 .quadradoInfo {
@@ -85,26 +166,26 @@ padding: 50px;
 }
 
 
-@media screen and (max-width: 1200px) {
+@media screen and (max-width: 1040px) {
     .main {
-        grid-template-columns: repeat(3, minmax(250px, 1fr)); /* 3 itens por linha */
+        grid-template-columns: repeat(2, minmax(250px, 1fr)); 
        
     }
 }
 
 @media screen and (max-width: 800px) {
     .main {
-        grid-template-columns: repeat(2, minmax(300px, 1fr)); /* 2 itens por linha */
-        margin: 0%;
+        grid-template-columns: repeat(2, minmax(250px, 1fr)); 
+
         align-items: center;
         
     }
 }
 
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 700px) {
     .main {
       margin: 0;
-        grid-template-columns: repeat(1, minmax(400px, 1fr)); /* 1 item por linha */
+        grid-template-columns: repeat(1, minmax(20px, 1fr)); 
     }
 }
 </style>
