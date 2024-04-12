0ace446f8888cee41aaa114c99083b7667cbd492
TomazMPP <tomazmppontes@gmail.com>
2024-04-12 20:41:09 -0300

    Update Check.vue

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -3,6 +3,24 @@
  <p class="warning">
 This page showcases the heroes available for purchase from the site owner,<br>always offered at competitive prices and ensuring a safe transaction.</p>
     <div class="main">
+      <div class="quadradoInfo">
+       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fpugdog_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 70px; padding: 3%;">  
+       <p><strong>ID: </strong>921544</p>
+       <p><strong>Rarity: </strong>99% Perfect Super Legend</p>
+       <p><strong>Price: </strong>65 MATIC</p>
+       <p class="abilit"><strong>P: 💥</strong> <u class="yelnun">17</u> | <strong>ST: <img src="https://bcrypt.com.br/_next/image?url=%2Fskills%2Fskill5.webp&w=32&q=75" alt="Speed Skill" style="width: 13px;"> </strong> <u class="yelun">17</u> | <strong>SP: <img src="https://bcrypt.com.br/_next/image?url=%2Fskills%2Fskill1.webp&w=32&q=75" alt="Speed Skill" style="width: 15px;"></strong> <u class="yelun">17</u></p>
+       <div class="skills">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fjail_breaker_icon.png&w=48&q=75" alt="Jail Breaker Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fpierce_block_icon.png&w=48&q=75" alt="Pierce Block Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fsave_battery_icon.png&w=48&q=75" alt="Save Battery Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ffast_charge_icon.png&w=48&q=75" alt="Battery Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fbomb_pass_icon.png&w=48&q=75" alt="Bomb Pass Skill">
+        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fblock_pass_icon.png&w=48&q=75" alt="Block Pass Skill">
+
+    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
+  </div>
+    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/921544" target="_blank">BUY NOW! (OPENSEA)</a>
+      </div>
       <div class="lendario quadradoInfo">
        <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fvampire_purple_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 70px; padding: 3%;">  
        <p><strong>ID: </strong>2320579</p>
