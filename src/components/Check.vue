622229d05a2656dee29ad19beda67e697da2f6e4
TomazMPP <tomazmppontes@gmail.com>
2024-04-13 20:12:07 -0300

    Update Check.vue

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -72,22 +72,6 @@ This page showcases the heroes available for purchase from the site owner,<br>al
     <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/3280285" target="_blank">BUY NOW! (OPENSEA)</a>
       </div>
       <div class="quadradoEpico quadradoInfo">
-       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fking_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
-       <p><strong>ID: </strong>1329468</p>
-       <p><strong>Rarity: </strong>Epic</p>
-       <p><strong>Price: </strong>7 MATIC</p>
-       <p class="abilit"><strong>P: 💥</strong> <u class="yelun">9</u> | <strong>ST: <img src="https://bcrypt.com.br/_next/image?url=%2Fskills%2Fskill5.webp&w=32&q=75" alt="Speed Skill" style="width: 13px;"> </strong> <u class="yelun">10</u> | <strong>SP: <img src="https://bcrypt.com.br/_next/image?url=%2Fskills%2Fskill1.webp&w=32&q=75" alt="Speed Skill" style="width: 15px;"></strong> <u class="yelun">10</u></p>
-       <div class="skills">
-        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ftreasure_hunter_icon.png&w=48&q=75" alt="Treasure Hunter Skill">
-        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fsave_battery_icon.png&w=48&q=75" alt="Save Battery Skill">
-        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ffast_charge_icon.png&w=48&q=75" alt="Battery Skill">
-        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fbomb_pass_icon.png&w=48&q=75" alt="Bomb Pass Skill">
-
-    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
-  </div>
-    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/1329468" target="_blank">BUY NOW! (OPENSEA)</a>
-      </div>
-      <div class="quadradoEpico quadradoInfo">
        <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Ftiger_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
        <p><strong>ID: </strong>835570</p>
        <p><strong>Rarity: </strong>Epic</p>
