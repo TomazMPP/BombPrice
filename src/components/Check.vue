e0c8f350c1c7557c04c3993aad2fe08acc9b4b85
TomazMPP <tomazmppontes@gmail.com>
2024-04-14 12:39:19 -0300

    Update Check.vue

--- src/components/Check.vue
+++ src/components/Check.vue
@@ -114,20 +114,6 @@ This page showcases the heroes available for purchase from the site owner,<br>al
   </div>
     <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/962565" target="_blank">BUY NOW! (OPENSEA)</a>
       </div>
-      <div class="quadradoSRaro quadradoInfo">
-       <img src="https://bcrypt.com.br/_next/image?url=%2Fbhero%2Fninja_icon.png&w=128&q=75" alt="Boneco do BombCrypto" style="width: 75px; padding: 5%;">  
-       <p><strong>ID: </strong>696689</p>
-       <p><strong>Rarity: </strong>Super Rare</p>
-       <p><strong>Price: </strong>2.5 MATIC</p>
-       <p class="abilit"><strong>P: 💥</strong> <u class="yelun">8</u> | <strong>ST: <img src="https://bcrypt.com.br/_next/image?url=%2Fskills%2Fskill5.webp&w=32&q=75" alt="Speed Skill" style="width: 13px;"> </strong> <u class="yelun">7</u> | <strong>SP: <img src="https://bcrypt.com.br/_next/image?url=%2Fskills%2Fskill1.webp&w=32&q=75" alt="Speed Skill" style="width: 15px;"></strong> <u class="yelun">8</u></p>
-       <div class="skills">
-        <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fjail_breaker_icon.png&w=48&q=75" alt="Jail Breaker Skill">
-      <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Ffast_charge_icon.png&w=48&q=75" alt="Battery Skill">
-      <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fbomb_pass_icon.png&w=48&q=75" alt="Bomb Pass Skill">
-    <img src="https://bcrypt.com.br/_next/image?url=%2Fatts%2Fshield_icon.png&w=48&q=75" alt="Shield Skill">
-  </div>
-    <a class="buyButton" href="https://opensea.io/assets/matic/0xd8a06936506379dbbe6e2d8ab1d8c96426320854/696689" target="_blank">BUY NOW! (OPENSEA)</a>
-      </div>
     </div>
   </div>
   </template>
